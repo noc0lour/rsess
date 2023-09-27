2771c3ca21db8e1b24d06829c23d864df28ccce0
Andrej Rode <mail@andrejro.de>
2023-09-27 17:35:28 +0200

    pyrsess: Add tests for encoding and decoding with ESS & OESS
    Tests follow the reference encoding for E_max=28, n_max=4, and
    8-ASK. Which we know the encoding from many publications.

---
+++ pyrsess/tests/pyrsess/test_oess.py
@@ -0,0 +1,78 @@
+"""
+Tests for the OESS class
+"""
+
+import pyrsess
+import numpy as np
+
+
+def test_encode():
+    ess = pyrsess.OESS(28, 4, 8)
+    sequence = ess.encode([0, 1, 0, 1])
+    assert len(sequence) == 4
+
+
+def test_multi_encode():
+    ess = pyrsess.OESS(28, 4, 8)
+    sequences = ess.multi_encode(
+        [[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [0, 0, 1, 1]]
+    )
+    assert sequences.shape == (4, 4)
+
+
+def test_construction_reference_encode():
+    ess = pyrsess.OESS(28, 4, 8)
+    nums = np.arange(16)
+    bin_nums = ((nums.reshape(-1, 1) & (2 ** np.flip(np.arange(4)))) != 0).astype(int)
+    sequences = ess.multi_encode(bin_nums)
+    assert (
+        sequences
+        == np.array(
+            [
+                [1, 1, 1, 1],
+                [1, 1, 1, 3],
+                [1, 1, 3, 1],
+                [1, 1, 3, 3],
+                [1, 3, 1, 1],
+                [1, 3, 1, 3],
+                [1, 3, 3, 1],
+                [3, 1, 1, 1],
+                [3, 1, 1, 3],
+                [3, 1, 3, 1],
+                [3, 3, 1, 1],
+                [1, 1, 1, 5],
+                [1, 1, 5, 1],
+                [1, 3, 3, 3],
+                [1, 5, 1, 1],
+                [3, 1, 3, 3],
+            ]
+        )
+    ).all()
+
+
+def test_construction_reference_decode():
+    ess = pyrsess.OESS(28, 4, 8)
+    nums = np.arange(16)
+    bin_nums = ((nums.reshape(-1, 1) & (2 ** np.flip(np.arange(4)))) != 0).astype(int)
+    sequences = np.array(
+        [
+            [1, 1, 1, 1],
+            [1, 1, 1, 3],
+            [1, 1, 3, 1],
+            [1, 1, 3, 3],
+            [1, 3, 1, 1],
+            [1, 3, 1, 3],
+            [1, 3, 3, 1],
+            [3, 1, 1, 1],
+            [3, 1, 1, 3],
+            [3, 1, 3, 1],
+            [3, 3, 1, 1],
+            [1, 1, 1, 5],
+            [1, 1, 5, 1],
+            [1, 3, 3, 3],
+            [1, 5, 1, 1],
+            [3, 1, 3, 3],
+        ]
+    )
+    bits = ess.multi_decode(sequences)
+    assert (bits == bin_nums).all()
